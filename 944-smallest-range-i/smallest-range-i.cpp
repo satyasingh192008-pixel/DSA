9741a02481766482194075cc5dc6f67cea3b9ee1
satyasingh192008-pixel <satyasingh192008@gmail.com>
2026-09-15 00:26:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (58.39%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.cpp
+++ 944-smallest-range-i/smallest-range-i.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
-        int mn = nums[0];
-        int mx = nums[0];
-
-        for(int i = 0; i < nums.size(); i++) {
-            mn = min(mn, nums[i]);
-            mx = max(mx, nums[i]);
-        }
-
-        return max(0, mx - mn - 2 * k);
+      sort(nums.begin(),nums.end());
+      int n=nums.size()-1;
+      int mn=nums[0];
+      int mx=nums[n] ;
+      return max(0,mx-mn-2*k); 
     }
 };
