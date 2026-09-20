38cc849a8b82a8d619e9d87df502fe70c4a5ffb6
satyasingh192008-pixel <satyasingh192008@gmail.com>
2026-09-20 13:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (95.77%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = 'z' - s[i] + 1;
+            int position = i + 1;
+
+            sum += value * position;
+        }
+
+        return sum;
+    }
+};
